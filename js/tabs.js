bf3cd7f58fca48266c331d9cba92f1f3acdeeed4
wondershineone <wondershine.one@gmail.com>
2022-12-22 22:49:25 +0900

    sanitize css

--- js/tabs.js
+++ js/tabs.js
@@ -1,43 +1,26 @@
-const removeActiveClasses = function (ulElement) {
-    const lis = ulElement.querySelectorAll('li');
-    Array.prototype.forEach.call(lis, function(li) {
-        li.classList.remove('active');
-    });
-}
-
-const getChildPosition = function (element) {
-    var parent = element.parentNode;
-    var i = 0;
-    for (var i = 0; i < parent.children.length; i++) {
-        if (parent.children[i] === element) {
-            return i;
-        }
-    }
-
-    throw new Error('No parent found');
-}
-
 window.addEventListener('load', function () {
-    const tabLinks = document.querySelectorAll('ul.tab li a');
-
-    Array.prototype.forEach.call(tabLinks, function(link) {
+    const tabLinks = document.querySelectorAll('#home-top-nav > li > a');
+    Array.prototype.forEach.call(tabLinks, function (link) {
         link.addEventListener('click', function (event) {
             event.preventDefault();
-
-            liTab = link.parentNode;
-            ulTab = liTab.parentNode;
-            position = getChildPosition(liTab);
-            if (liTab.className.includes('active')) {
-                return;
+            let liTab = link.parentNode;
+            let allTabsId = liTab.dataset.tabs;
+            if (allTabsId) {
+                let tabs = document.getElementById(allTabsId);
+                Array.prototype.forEach.call(tabs.children, function (tab) {
+                    if (!tab.className.includes("hide")) {
+                        tab.classList.add('hide');
+                    }
+                })
             }
 
-            removeActiveClasses(ulTab);
-            tabContentId = ulTab.getAttribute('data-tab');
-            tabContentElement = document.getElementById(tabContentId);
-            removeActiveClasses(tabContentElement);
-
-            tabContentElement.querySelectorAll('li')[position].classList.add('active');
-            liTab.classList.add('active');
+            let tabId = liTab.dataset.tab;
+            if (tabId) {
+                let targetTab = document.getElementById(tabId);
+                if (targetTab.className.includes("hide")) {
+                    targetTab.classList.remove("hide");
+                }
+            }
         }, false);
     });
 });
